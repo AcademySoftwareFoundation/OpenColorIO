5faf856c7256c18542ed14ff61a20a4746386215
Mark Fickett <mfseas@gmail.com>
2010-06-17 14:53:45 -0700

    Fix compilation on Python 2.5.

--- src/pyglue/PyGroupTransform.cpp
+++ src/pyglue/PyGroupTransform.cpp
@@ -245,7 +245,9 @@ OCS_NAMESPACE_ENTER
         0,                                          //tp_subclasses
         0,                                          //tp_weaklist
         0,                                          //tp_del
+        #if PY_VERSION_HEX > 0x02060000
         0,                                          //tp_version_tag
+        #endif
     };
     
     ///////////////////////////////////////////////////////////////////////////
