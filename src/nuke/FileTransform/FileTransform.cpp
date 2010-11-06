745a7c495b4b3e5ab3faab17e5a40dbb3325d4f0
Jeremy Selan <jeremy.selan@gmail.com>
2010-11-05 17:06:56 -0700

    Added FileTransform node for nuke to allow for individual lut application.

---
+++ src/nuke/FileTransform/FileTransform.cpp
@@ -0,0 +1,191 @@
+/**
+ * OpenColorIO conversion Iop.
+ */
+
+#include "FileTransform.h"
+
+namespace OCIO = OCIO_NAMESPACE;
+
+#include <DDImage/PixelIop.h>
+#include <DDImage/NukeWrapper.h>
+#include <DDImage/Row.h>
+#include <DDImage/Knobs.h>
+
+#include <string>
+#include <sstream>
+#include <stdexcept>
+
+
+/*
+const char* FileTransform::intepolation[] = {
+    "unknown", "nearest", "linear", 0
+};
+*/
+
+FileTransform::FileTransform(Node *n) : DD::Image::PixelIop(n)
+{
+    src = NULL;
+    
+    layersToProcess = DD::Image::Mask_RGB;
+
+}
+
+FileTransform::~FileTransform()
+{
+
+}
+
+void FileTransform::knobs(DD::Image::Knob_Callback f)
+{
+
+    File_knob(f, &src, "src", "src");
+    DD::Image::Tooltip(f, "Specify the src file, on disk, to use for this transform.");
+    
+    DD::Image::Divider(f);
+    
+    DD::Image::Input_ChannelSet_knob(f, &layersToProcess, 0, "layer", "layer");
+    DD::Image::SetFlags(f, DD::Image::Knob::NO_CHECKMARKS | DD::Image::Knob::NO_ALPHA_PULLDOWN);
+    DD::Image::Tooltip(f, "Set which layer to process. This should be a layer with rgb data.");
+}
+
+void FileTransform::_validate(bool for_real)
+{
+    input0().validate(for_real);
+    
+    if(!src)
+    {
+        error("The source file must be specified.");
+        return;
+    }
+    
+    try
+    {
+        OCIO::ConstConfigRcPtr config = OCIO::GetCurrentConfig();
+        
+        OCIO::FileTransformRcPtr transform = OCIO::FileTransform::Create();
+        transform->setSrc(src);
+        transform->setInterpolation(OCIO::INTERP_LINEAR);
+        
+        processor = config->getProcessor(transform);
+    }
+    catch(OCIO::Exception &e)
+    {
+        error(e.what());
+        return;
+    }
+    
+    if(processor->isNoOp())
+    {
+        // TODO or call disable() ?
+        set_out_channels(DD::Image::Mask_None); // prevents engine() from being called
+        copy_info();
+        return;
+    }
+    
+    set_out_channels(DD::Image::Mask_All);
+
+    DD::Image::PixelIop::_validate(for_real);
+}
+
+// Note that this is copied by others (OCIODisplay)
+void FileTransform::in_channels(int /* n unused */, DD::Image::ChannelSet& mask) const
+{
+    DD::Image::ChannelSet done;
+    foreach(c, mask)
+    {
+        if ((layersToProcess & c) && DD::Image::colourIndex(c) < 3 && !(done & c))
+        {
+            done.addBrothers(c, 3);
+        }
+    }
+    mask += done;
+}
+
+// See Saturation::pixel_engine for a well-commented example.
+// Note that this is copied by others (OCIODisplay)
+void FileTransform::pixel_engine(
+    const DD::Image::Row& in,
+    int /* rowY unused */, int rowX, int rowXBound,
+    const DD::Image::ChannelMask outputChannels,
+    DD::Image::Row& out)
+{
+    int rowWidth = rowXBound - rowX;
+
+    DD::Image::ChannelSet done;
+    foreach (requestedChannel, outputChannels)
+    {
+        // Skip channels which had their trios processed already,
+        if (done & requestedChannel)
+        {
+            continue;
+        }
+
+        // Pass through channels which are not selected for processing
+        // and non-rgb channels.
+        if (!(layersToProcess & requestedChannel) || colourIndex(requestedChannel) >= 3)
+        {
+            out.copy(in, requestedChannel, rowX, rowXBound);
+            continue;
+        }
+
+        DD::Image::Channel rChannel = DD::Image::brother(requestedChannel, 0);
+        DD::Image::Channel gChannel = DD::Image::brother(requestedChannel, 1);
+        DD::Image::Channel bChannel = DD::Image::brother(requestedChannel, 2);
+
+        done += rChannel;
+        done += gChannel;
+        done += bChannel;
+
+        const float *rIn = in[rChannel] + rowX;
+        const float *gIn = in[gChannel] + rowX;
+        const float *bIn = in[bChannel] + rowX;
+
+        float *rOut = out.writable(rChannel) + rowX;
+        float *gOut = out.writable(gChannel) + rowX;
+        float *bOut = out.writable(bChannel) + rowX;
+
+        // OCIO modifies in-place
+        memcpy(rOut, rIn, sizeof(float)*rowWidth);
+        memcpy(gOut, gIn, sizeof(float)*rowWidth);
+        memcpy(bOut, bIn, sizeof(float)*rowWidth);
+
+        try
+        {
+            OCIO::PlanarImageDesc img(rOut, gOut, bOut, rowWidth, /*height*/ 1);
+            processor->apply(img);
+        }
+        catch(OCIO::Exception &e)
+        {
+            error(e.what());
+        }
+    }
+}
+
+const DD::Image::Op::Description FileTransform::description("OCIOFileTransform", build);
+
+const char* FileTransform::Class() const
+{
+    return description.name;
+}
+
+const char* FileTransform::displayName() const
+{
+    return description.name;
+}
+
+const char* FileTransform::node_help() const
+{
+    // TODO more detailed help text
+    return "Use OpenColorIO to apply the specified LUT file transform.";
+}
+
+
+DD::Image::Op* build(Node *node)
+{
+    DD::Image::NukeWrapper *op = new DD::Image::NukeWrapper(new FileTransform(node));
+    op->noMix();
+    op->noMask();
+    op->noChannels(); // prefer our own channels control without checkboxes / alpha
+    op->noUnpremult();
+    return op;
+}
---
+++ src/nuke/FileTransform/FileTransform.h
@@ -0,0 +1,111 @@
+#ifndef INCLUDED_OCIO_NUKE_FILETRANSFORM_H_
+#define INCLUDED_OCIO_NUKE_FILETRANSFORM_H_
+
+// Include these early, for Nuke's headers under gcc 4.4.2.
+#include <memory>
+#include <cstdarg>
+
+#include <DDImage/PixelIop.h>
+#include <DDImage/Row.h>
+#include <DDImage/Knob.h>
+
+#include <OpenColorIO/OpenColorIO.h>
+namespace OCIO = OCIO_NAMESPACE;
+
+
+/*!
+ * Iop that uses OpenColorIO to perform colorspace conversions
+ */
+class FileTransform : public DD::Image::PixelIop {
+
+    protected:
+        DD::Image::ChannelSet layersToProcess; //!< layers (rgb channel groups) to process
+        //int modeindex;
+        const char* src;
+        
+        /*
+        int inputColorSpaceIndex; //!< index of input colorspace selection from the pulldown list knob
+        int outputColorSpaceIndex;
+        std::vector<std::string> colorSpaceNames; //!< list of input and output colorspace names (memory for const char* s below)
+        std::vector<const char*> inputColorSpaceCstrNames; //!< list for the pulldown list knob (used raw)
+        std::vector<const char*> outputColorSpaceCstrNames;
+        */
+        
+        OCIO::ConstProcessorRcPtr processor;
+    public:
+        // static const char* modes[];
+
+        FileTransform(Node *node);
+
+        ~FileTransform();
+
+        static const DD::Image::Op::Description description;
+
+        /*! Return the command name that will be stored in Nuke scripts. */
+        const char *Class() const;
+
+        /*!
+         * Return a name for this class that will be shown to the user. The
+         * default implementation returns Class(). You can return a different
+         * (ie more user-friendly) name instead here, and there is no need for
+         * this to be unique.
+         * 
+         * Nuke currently will remove any trailing digits and underscores from
+         * this and add a new number to make a unique name for the new node.
+         * 
+         * \return "OCIOFileTransform"
+         */
+        const char *displayName() const;
+
+        /*!
+         * Return help information for this node. This information is in the
+         * pop-up window that the user gets when they hit the [?] button in
+         * the lower-left corner of the control panel.
+         */
+        const char *node_help() const;
+
+        /*!
+         * Define the knobs that will be presented in the control panel.
+         */
+        void knobs(DD::Image::Knob_Callback f);
+
+        /*!
+         * Specify the channels required from input n to produce the channels
+         * in mask by modifying mask in-place. (At least one channel in the
+         * input is assumed.)
+         *
+         * Since FileTransform conversions can have channel cross-talk, any rgb
+         * output channel requires all its rgb bretheren. (Non-rgb
+         * are passed through.)
+         */
+        void in_channels(int n, DD::Image::ChannelSet& mask) const;
+
+        /*!
+         * Calculate the output pixel data.
+         * \param rowY vertical line number
+         * \param rowX inclusive left bound
+         * \param rowXBound exclusive right bound
+         * \param outputChannels a subset of out_channels(), the required channels to be produced
+         */
+        void pixel_engine(
+            const DD::Image::Row& in,
+            int rowY, int rowX, int rowXBound,
+            const DD::Image::ChannelMask outputChannels,
+            DD::Image::Row& out);
+
+
+    protected:
+
+        /*!
+         * Check that colorspaces are available, and that the transform
+         * is not a noop. (As OCIO whether a given transform is a noop, since it
+         * can do more analysis than just name matching.)
+         */
+        void _validate(bool for_real);
+
+};
+
+
+static DD::Image::Op* build(Node *node);
+
+#endif // INCLUDED_OCIO_NUKE_FILETRANSFORM_H_
