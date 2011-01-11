8151eca9edf3acf027e2a60ea3857f2240af6461
dbr <dbr.onix@gmail.com>
2011-01-11 20:45:35 +1030

    Tidier Nuke menu.py, and register viewer processes
    ocio_menu adds all the OCIO plugins to the Color/OCIO/ menu (with an all-important icon)
    
    ocio_viewer registers a OCIODisplay node for each display device and transform (e.g "Film1D (sRGB)", "Log (sRGB)", "Film1D (P3)", "Log (P3)")

--- share/nuke/menu.py
+++ share/nuke/menu.py
@@ -1,14 +1,62 @@
+import os
+import nuke
 
-toolbar = nuke.toolbar('Nodes')
+def ocio_menu():
+    """Adds OCIO nodes to a menu in Color
+    """
 
-if nuke.pluginExists( 'OCIOColorSpace' ):
-    toolbar.addCommand( 'Color/OCIOColorSpace',"nuke.createNode('OCIOColorSpace')")
+    m_nodes = nuke.toolbar('Nodes')
+    m_color = m_nodes.findItem("Color")
+    m_ocio = m_color.addMenu("OCIO", icon = "ocio_icon.png")
 
-if nuke.pluginExists( 'OCIODisplay' ):
-    toolbar.addCommand( 'Color/OCIODisplay',"nuke.createNode('OCIODisplay')")
+    allplugs = nuke.plugins(nuke.ALL | nuke.NODIR, "OCIO*.so", "OCIO*.dylib", "OCIO*.dll")
 
-if nuke.pluginExists( 'OCIOFileTransform' ):
-    toolbar.addCommand( 'Color/OCIOFileTransform',"nuke.createNode('OCIOFileTransform')")
+    for fname in allplugs:
+        p = os.path.splitext(fname)[0] # strip .so extension
+        m_ocio.addCommand(p, lambda p=p: nuke.createNode(p))
 
-if nuke.pluginExists( 'OCIOLogConvert' ):
-    toolbar.addCommand( 'Color/OCIOLogConvert',"nuke.createNode('OCIOLogConvert')")
+
+def ocio_viewer(remove_nuke_default = True):
+    """Registers the a viewer process for each display/transform, and
+    sets the default
+
+    Also unregisters the default Nuke viewer processes (sRGB/rec709)
+    unless remove_nuke_default is False
+    """
+
+    if remove_nuke_default:
+        nuke.ViewerProcess.unregister('rec709')
+        nuke.ViewerProcess.unregister('sRGB')
+
+
+    # Formats the display and transform, e.g "Film1D (sRGB"
+    DISPLAY_UI_FORMAT = "%(transform)s (%(display)s)"
+
+    import PyOpenColorIO as OCIO
+    cfg = OCIO.GetCurrentConfig()
+
+    allDisplays = cfg.getDisplayDeviceNames()
+
+    # For every display, loop over every transform
+    for dname in allDisplays:
+        allTransforms = cfg.getDisplayTransformNames(dname)
+
+        for xform in allTransforms:
+            nuke.ViewerProcess.register(
+                name = DISPLAY_UI_FORMAT % {'transform': xform, "display": dname},
+                call = nuke.nodes.OCIODisplay,
+                args = (),
+                kwargs = {"device": dname, "transform": xform})
+
+
+    defaultDisplay = cfg.getDefaultDisplayDeviceName()
+    defaultXform = cfg.getDefaultDisplayTransformName(defaultDisplay)
+    
+    nuke.knobDefault(
+        "Viewer.viewerProcess",
+        DISPLAY_UI_FORMAT % {'transform': defaultXform, "display": defaultDisplay})
+
+
+if __name__ == "__main__":
+    ocio_menu()
+    ocio_viewer()
