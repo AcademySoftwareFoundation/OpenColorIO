76cdddfb18364683cb3caf406607e68f136bb0cb
dbr <dbr.onix@gmail.com>
2011-04-03 00:17:58 +1030

    Basic version of the CDLTransform Nuke node, closes #60

---
+++ src/nuke/CDLTransform/CDLTransform.cpp
@@ -0,0 +1,197 @@
+/**
+ * OpenColorIO conversion Iop.
+ */
+
+#include "CDLTransform.h"
+
+namespace OCIO = OCIO_NAMESPACE;
+
+#include <DDImage/PixelIop.h>
+#include <DDImage/NukeWrapper.h>
+#include <DDImage/Row.h>
+#include <DDImage/Knobs.h>
+
+#include <string>
+#include <sstream>
+#include <stdexcept>
+
+
+CDLTransform::CDLTransform(Node *n) : DD::Image::PixelIop(n)
+{
+    layersToProcess = DD::Image::Mask_RGB;
+
+    for (int i = 0; i < 3; i++){
+        m_slope[i] = 1.0;
+        m_offset[i] = 0.0;
+        m_power[i] = 1.0;
+    }
+
+    m_saturation = 1.0;
+
+    m_cccid = "";
+}
+
+CDLTransform::~CDLTransform()
+{
+
+}
+
+void CDLTransform::knobs(DD::Image::Knob_Callback f)
+{
+
+    DD::Image::Color_knob(f, m_slope, DD::Image::IRange(0, 4.0), "slope");
+    DD::Image::Color_knob(f, m_offset, DD::Image::IRange(-0.2, 0.2), "offset");
+    DD::Image::Color_knob(f, m_power, DD::Image::IRange(0.0, 4.0), "power");
+    DD::Image::Float_knob(f, &m_saturation, DD::Image::IRange(0, 4.0), "saturation");
+    
+    DD::Image::Divider(f);
+
+    DD::Image::String_knob(f, &m_cccid, "cccid", "cccid");
+
+    DD::Image::Divider(f);
+    
+    DD::Image::Input_ChannelSet_knob(f, &layersToProcess, 0, "layer", "layer");
+    DD::Image::SetFlags(f, DD::Image::Knob::NO_CHECKMARKS | DD::Image::Knob::NO_ALPHA_PULLDOWN);
+    DD::Image::Tooltip(f, "Set which layer to process. This should be a layer with rgb data.");
+}
+
+void CDLTransform::_validate(bool for_real)
+{
+    input0().validate(for_real);
+    
+    try
+    {
+        OCIO::ConstConfigRcPtr config = OCIO::GetCurrentConfig();
+        config->sanityCheck();
+
+        OCIO::CDLTransformRcPtr cc = OCIO::CDLTransform::Create();
+        cc->setSlope(m_slope);
+        cc->setOffset(m_offset);
+        cc->setPower(m_power);
+        cc->setSat(m_saturation);
+        
+        m_processor = config->getProcessor(cc);
+    }
+    catch(OCIO::Exception &e)
+    {
+        error(e.what());
+        return;
+    }
+    
+    if(m_processor->isNoOp())
+    {
+        // TODO or call disable() ?
+        set_out_channels(DD::Image::Mask_None); // prevents engine() from being called
+        copy_info();
+        return;
+    }
+    
+    set_out_channels(DD::Image::Mask_All);
+
+    DD::Image::PixelIop::_validate(for_real);
+}
+
+// Note that this is copied by others (OCIODisplay)
+void CDLTransform::in_channels(int /* n unused */, DD::Image::ChannelSet& mask) const
+{
+    DD::Image::ChannelSet done;
+    foreach(c, mask)
+    {
+        if ((layersToProcess & c) && DD::Image::colourIndex(c) < 3 && !(done & c))
+        {
+            done.addBrothers(c, 3);
+        }
+    }
+    mask += done;
+}
+
+// See Saturation::pixel_engine for a well-commented example.
+// Note that this is copied by others (OCIODisplay)
+void CDLTransform::pixel_engine(
+    const DD::Image::Row& in,
+    int /* rowY unused */, int rowX, int rowXBound,
+    const DD::Image::ChannelMask outputChannels,
+    DD::Image::Row& out)
+{
+    int rowWidth = rowXBound - rowX;
+
+    DD::Image::ChannelSet done;
+    foreach (requestedChannel, outputChannels)
+    {
+        // Skip channels which had their trios processed already,
+        if (done & requestedChannel)
+        {
+            continue;
+        }
+
+        // Pass through channels which are not selected for processing
+        // and non-rgb channels.
+        if (!(layersToProcess & requestedChannel) || colourIndex(requestedChannel) >= 3)
+        {
+            out.copy(in, requestedChannel, rowX, rowXBound);
+            continue;
+        }
+
+        DD::Image::Channel rChannel = DD::Image::brother(requestedChannel, 0);
+        DD::Image::Channel gChannel = DD::Image::brother(requestedChannel, 1);
+        DD::Image::Channel bChannel = DD::Image::brother(requestedChannel, 2);
+
+        done += rChannel;
+        done += gChannel;
+        done += bChannel;
+
+        const float *rIn = in[rChannel] + rowX;
+        const float *gIn = in[gChannel] + rowX;
+        const float *bIn = in[bChannel] + rowX;
+
+        float *rOut = out.writable(rChannel) + rowX;
+        float *gOut = out.writable(gChannel) + rowX;
+        float *bOut = out.writable(bChannel) + rowX;
+
+        // OCIO modifies in-place
+        memcpy(rOut, rIn, sizeof(float)*rowWidth);
+        memcpy(gOut, gIn, sizeof(float)*rowWidth);
+        memcpy(bOut, bIn, sizeof(float)*rowWidth);
+
+        try
+        {
+            OCIO::PlanarImageDesc img(rOut, gOut, bOut, rowWidth, /*height*/ 1);
+            m_processor->apply(img);
+        }
+        catch(OCIO::Exception &e)
+        {
+            error(e.what());
+        }
+    }
+}
+
+const DD::Image::Op::Description CDLTransform::description("OCIOCDLTransform", build);
+
+const char* CDLTransform::Class() const
+{
+    return description.name;
+}
+
+const char* CDLTransform::displayName() const
+{
+    return description.name;
+}
+
+const char* CDLTransform::node_help() const
+{
+    // TODO more detailed help text
+    return "Use OpenColorIO to apply an ASC CDL grade. Applied using:\n\n"\
+        "out = (i * s + o)^p\n\nWhere i is the input value, s is slope, "\
+        "o is offset and p is power";
+}
+
+
+DD::Image::Op* build(Node *node)
+{
+    DD::Image::NukeWrapper *op = new DD::Image::NukeWrapper(new CDLTransform(node));
+    op->noMix();
+    op->noMask();
+    op->noChannels(); // prefer our own channels control without checkboxes / alpha
+    op->noUnpremult();
+    return op;
+}
