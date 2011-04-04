25e47590db224f2976a48cab843b6de1d1b14986
dbr <dbr.onix@gmail.com>
2011-04-04 22:41:54 +0930

    Build Nuke stuff into lib/nuke${MAJOR}.${MINOR} (e.g lib/nuke/6.2)
    As described in comment, Nuke maintains compatibility between "v" releases, meaning a build for 6.2v1 will work for 6.2v2, 6.2v3 etc

--- share/cmake/TestForDDImageVersion.cxx
+++ share/cmake/TestForDDImageVersion.cxx
@@ -2,6 +2,13 @@
 #include "DDImage/ddImageVersionNumbers.h"
 int main(int, char*[])
 {
-  std::cout << kDDImageVersion;
-  return 0;
+    // Print a Nuke API identifier number, used to make the
+    // lib/nuke${API_NUMBER} directory
+
+    // Nuke aims to maintain API compatibilty between "v" releases, so
+    // compiling for 6.1v1 will work with 6.1v2 etc (but not
+    // 6.2v1). Only exception has been 5.1v5 and 5.1v6 (because it was
+    // supposed to be 5.2v1)
+    std::cout << kDDImageVersionMajorNum << "." << kDDImageVersionMinorNum;
+    return 0;
 }
