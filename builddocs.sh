b67d2e3aa8bf7101116455a50e2b512dc4784203
Jeremy Selan <jeremy.selan@gmail.com>
2011-05-12 08:53:32 -0700

    added script to rebuild docs/website

---
+++ builddocs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Clone master branch into sub-directory
+mkdir -p mastercopy
+git archive master | tar -x -C mastercopy
+
+# Run cmake in a build directory
+mkdir -p build
+cd build
+cmake -D MAKE_DOCS=YES ../mastercopy
+
+# Build the docs
+make doc
+
+# Currently in build dir, move HTML up one level
+cp -R docs/build-html/* ..
+
