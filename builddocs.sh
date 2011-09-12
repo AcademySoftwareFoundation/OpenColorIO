67be3c9f1cf9ebdfc8ea7cfd33f95214c66cbcb8
Jeremy Selan <jeremy.selan@gmail.com>
2011-09-12 10:35:21 -0700

    updated docs build script

--- builddocs.sh
+++ builddocs.sh
@@ -7,7 +7,14 @@ git archive master | tar -x -C mastercopy
 # Run cmake in a build directory
 mkdir -p build
 cd build
-cmake -D MAKE_DOCS=YES ../mastercopy
+cmake \
+-D OCIO_BUILD_DOCS=YES \
+-D OCIO_BUILD_STATIC=NO \
+-D OCIO_BUILD_STATIC=NO \
+-D OCIO_BUILD_APPS=NO \
+-D OCIO_BUILD_TESTS=NO \
+-D OCIO_BUILD_PYGLUE=NO \
+../mastercopy
 
 # Build the docs
 make doc -j8
