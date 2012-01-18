92dac1ada9850c792ae0518959740376b042ef7c
dbr <dbr.onix@gmail.com>
2012-01-19 00:02:46 +1030

    Extremely beta'y OCIO RV intergration functional
    RV OCIO menu and "d" shortcut work

--- src/rv/Python/ociorv.py
+++ src/rv/Python/ociorv.py
@@ -2,6 +2,8 @@ import time
 import itertools
 import contextlib
 
+import PyOpenColorIO as OCIO
+
 import rv
 from rv.commands import setStringProperty, setIntProperty, setFloatProperty
 
@@ -29,7 +31,7 @@ def set_lut(proc, nodename):
         # Assume crosstalk in lieu of latest OCIO Python bindings
         crosstalk = True
 
-    # FIXME: Hardcoded cube sizes
+    # TODO: Maybe allow configuration of LUT sizes?
     size_1d = 2048
     size_3d = 32
 
@@ -43,7 +45,7 @@ def set_lut(proc, nodename):
 
 
 def _set_lut_3d(proc, nodename, size = 32):
-    # FIXME: This clips with >0 scene-linear values, use allocation
+    # FIXME: This clips with >1 scene-linear values, use allocation
     # vars
 
     # Make noop cube
@@ -58,8 +60,6 @@ def _set_lut_3d(proc, nodename, size = 32):
     # Apply transform
     cube_processed = proc.applyRGB(cube_raw)
 
-    # TODO: Use allocation vars to set prelut
-
     # Set LUT type and size, then LUT values
     setStringProperty("%s.lut.type" % nodename, ["RGB"], False)
     setIntProperty("%s.lut.size" % nodename, [size, size, size], False)
@@ -70,6 +70,8 @@ def _set_lut_3d(proc, nodename, size = 32):
 
 
 def _set_lut_1d(proc, nodename, size = 1024):
+    # TODO: Use allocation vars also
+
     # Make noop ramp
     def gen_noop_ramp(size):
         size_minus_one = float(size-1)
@@ -103,54 +105,105 @@ def set_noop(nodename):
     setIntProperty("%s.lut.active" % nodename, [is_active], False)
 
 
+def view_to_uistr(view):
+    # sRGB/Film
+    return "/".join(view)
+
+
 class PyMyStuffMode(rv.rvtypes.MinorMode):
 
     def set_display(self, event):
-        rv.extra_commands.displayFeedback("Changing display", 2.0)
+        avail = self.get_views()
+        cur = self.active_view
+        curindex = avail.index(cur)
+        newindex = (curindex + 1) % len(avail)
+        self.active_view = avail[newindex]
+        rv.extra_commands.displayFeedback(
+            "Changing display to %s" % view_to_uistr(self.active_view),
+            2.0, # timeout
+            )
+        self.refresh()
+
+    def get_cfg(self):
+        return OCIO.GetCurrentConfig()
+
+    def refresh(self):
+        """Refresh LUT on all sources
+        """
+        for src in rv.commands.nodesOfType("RVSource"):
+            path = rv.commands.getStringProperty(
+                "%s.media.movie" % src,
+                0, 99999999)[0] # defaultish arguments
+            self._config_source(src = src, srcpath = path)
 
     def source_setup(self, event):
+        # Event content example:
+        # sourceGroup000001_source;;RVSource;;/path/to/myimg.exr
+        print event.contents()
         args = event.contents().split(";;")
         src = args[0]
-        #srctype = args[1]
         srcpath = args[2]
 
-        print "Colour stuff for %s" % srcpath
+        self._config_source(src = src, srcpath = srcpath)
 
+    def _config_source(self, src, srcpath):
         filelut_node = rv.extra_commands.associatedNode("RVColor", src)
         looklut_node = rv.extra_commands.associatedNode("RVLookLUT", src)
 
         # Set 3D LUT, and activate
-        import PyOpenColorIO as OCIO
-        cfg = OCIO.GetCurrentConfig()
+        cfg = self.get_cfg()
 
         # FIXME: Need a way to customise this per-facility without
         # modifying this file (try: import ociorv_custom_stuff ?)
         inspace = cfg.parseColorSpaceFromString(srcpath)
-        outspace = "srgb8" # nonsense
 
-        testproc = cfg.getProcessor(inspace, outspace)
+        test_transform = OCIO.DisplayTransform()
+        test_transform.setInputColorSpaceName(inspace)
+        display, view = self.active_view
+        test_transform.setDisplay(display)
+        test_transform.setView(view)
 
-        if testproc.isNoOp():
-            return
-
-        # Get processor from input space to scene-linear, so grading
-        # controls etc work as expected
         try:
-            input_proc = cfg.getProcessor(inspace, OCIO.Constants.ROLE_SCENE_LINEAR)
+            test_proc = cfg.getProcessor(test_transform)
         except OCIO.Exception, e:
-            print "INFO: Cannot linearise source %s - OCIO error was %s" % (
-                srcpath,
+            print "INFO: Cannot create test OCIODisplay for %s - display LUT disabled. OCIO message was %s" % (
+                inspace,
                 e)
             set_noop(nodename = filelut_node)
             set_noop(nodename = looklut_node)
             return
 
-        # Get processor form scene-linear to output space
+        if test_proc.isNoOp():
+            print "Source is NoOp"
+            set_noop(nodename = filelut_node)
+            set_noop(nodename = looklut_node)
+            return
+
+        # Input -> scene-linear processor. Allow grading controls etc
+        # to work as expected
         try:
-            output_proc = cfg.getProcessor(OCIO.Constants.ROLE_SCENE_LINEAR, outspace)
+            input_proc = cfg.getProcessor(inspace, OCIO.Constants.ROLE_SCENE_LINEAR)
         except OCIO.Exception, e:
-            print "INFO: Cannot apply scene-linear to %s %s - OCIO error was %s" % (
-                outspace,
+            # TODO: This mostly catches "3D Luts can only be applied
+            # in the forward direction.", could handle this better
+            print "INFO: Cannot linearise %s - display LUT disabled. OCIO message was %s" % (
+                inspace,
+                e)
+            set_noop(nodename = filelut_node)
+            set_noop(nodename = looklut_node)
+            return
+
+        # Scene-linear -> output display transform
+        transform = OCIO.DisplayTransform()
+        transform.setDisplay(self.active_view[0])
+        transform.setView(self.active_view[1])
+
+        transform.setInputColorSpaceName(OCIO.Constants.ROLE_SCENE_LINEAR)
+
+        try:
+            output_proc = cfg.getProcessor(transform)
+        except OCIO.Exception, e:
+            print "INFO: Cannot apply scene-linear to %s - OCIO error was %s" % (
                 srcpath,
                 e)
             set_noop(nodename = filelut_node)
@@ -166,6 +219,30 @@ class PyMyStuffMode(rv.rvtypes.MinorMode):
         # Update LUT
         rv.commands.updateLUT()
 
+    def set_view(self, display, view):
+        """Set view, and update all sources
+        """
+        print "display: %s, view: %s" % (display, view)
+        self.active_view = (display, view)
+        self.refresh()
+
+    def get_views(self):
+        """Return [("sRGB", "Film"), ("sRGB", "Log"), ...]
+        """
+        available_views = []
+
+        cfg = self.get_cfg()
+        for a_display in cfg.getDisplays():
+            if a_display not in cfg.getActiveDisplays().split(", "):
+                continue
+
+            for a_view in cfg.getViews(a_display):
+                if a_view not in cfg.getActiveViews():
+                    continue
+
+                available_views.append(
+                    (a_display, a_view))
+        return available_views
 
     def __init__(self):
         rv.rvtypes.MinorMode.__init__(self)
@@ -178,7 +255,28 @@ class PyMyStuffMode(rv.rvtypes.MinorMode):
             ]
 
         local_bindings = []
-        menu = []
+
+        # Create [("sRGB/Film", self.set_menu(...)), ("sRGB/Log", ...)]
+        views_menu = []
+        for cur_view in self.get_views():
+            def set_view(event, cur_view = cur_view):
+                display, view = cur_view
+                self.set_view(display = display, view = view)
+            mi = (
+                "/".join(cur_view), # label
+                set_view, # actionHook
+                )
+            views_menu.append(mi)
+
+        # Set default view
+        cfg = self.get_cfg()
+        self.active_view = (
+            cfg.getDefaultDisplay(),
+            cfg.getDefaultView(cfg.getDefaultDisplay()))
+
+        # Construct top-level menu
+        menu = [("OCIO", [
+                    ("Display/Views", views_menu)])]
 
         self.init(mode_name,
                   global_bindings,
