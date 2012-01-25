e2e026dad550982ddfaaaab113d0c0b298603628
Jeremy Selan <jeremy.selan@gmail.com>
2012-01-25 15:11:34 -0800

    bumped to yaml-cpp.0.3.0

--- ext/yaml-cpp_packagerev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SVNREV="r482"
-svn export -r ${SVNREV} http://yaml-cpp.googlecode.com/svn/trunk yaml-cpp-${SVNREV}
-tar -cvzf yaml-cpp-${SVNREV}.tar.gz yaml-cpp-${SVNREV}
-/bin/rm -rf yaml-cpp-${SVNREV}
