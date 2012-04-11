7457a1b0cac6d7be16a616f1cd056eea0025fef6
unknown <ScanlineVfx_user@vcpro1008.scanlinevfxvc.com>
2012-04-10 17:11:38 -0700

    fixed ColorSpaceTransform windows segfault
    ColorSpaceTransform.cpp: if used with no default config, ( -lut defined in the command line )
      the default color spaces have no allocation variables defined. So you get
      an out of bounds exception at trying to access .vars[0]

--- src/core/ColorSpaceTransform.cpp
+++ src/core/ColorSpaceTransform.cpp
@@ -199,7 +199,10 @@ OCIO_NAMESPACE_ENTER
         AllocationData srcAllocation;
         srcAllocation.allocation = srcColorSpace->getAllocation();
         srcAllocation.vars.resize( srcColorSpace->getAllocationNumVars());
-        srcColorSpace->getAllocationVars(&srcAllocation.vars[0]);
+        if(srcAllocation.vars.size() > 0)
+        {
+            srcColorSpace->getAllocationVars(&srcAllocation.vars[0]);
+        }
         
         CreateGpuAllocationNoOp(ops, srcAllocation);
         
@@ -232,7 +235,10 @@ OCIO_NAMESPACE_ENTER
         AllocationData dstAllocation;
         dstAllocation.allocation = dstColorSpace->getAllocation();
         dstAllocation.vars.resize( dstColorSpace->getAllocationNumVars());
-        dstColorSpace->getAllocationVars(&dstAllocation.vars[0]);
+        if(dstAllocation.vars.size() > 0)
+        {
+            dstColorSpace->getAllocationVars(&dstAllocation.vars[0]);
+        }
         
         CreateGpuAllocationNoOp(ops, dstAllocation);
     }
