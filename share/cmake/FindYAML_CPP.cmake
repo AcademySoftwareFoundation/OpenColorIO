3afc721a4b095c4040ba7d57a57c85289e48a7b5
Frederik Aalund <frederikaalund@gmail.com>
2014-09-12 14:57:49 +0200

    External YAML_CPP can now be found on Windows without PKG Config. Static builds now don't define any special linkage attributes. This fixes compilation using Visual Studio.

--- share/cmake/FindYAML_CPP.cmake
+++ share/cmake/FindYAML_CPP.cmake
@@ -6,8 +6,10 @@
 #  YAML_CPP_LIBRARIES - library to link against
 #  YAML_CPP_FOUND - true if YAML_CPP was found.
 
-find_path(YAML_CPP_INCLUDE_DIR yaml-cpp/yaml.h)
-find_library(YAML_CPP_LIBRARY NAMES yaml-cpp)
+find_path(YAML_CPP_INCLUDE_DIR yaml-cpp/yaml.h
+		  HINTS ${PC_YAML_CPP_INCLUDEDIR} ${PC_YAML_CPP_INCLUDE_DIRS})
+find_library(YAML_CPP_LIBRARY LIBRARY_NAMES yaml-cpp libyaml-cpp
+        HINTS ${PC_YAML_CPP_LIBRARY_DIRS} )
 
 # Support the REQUIRED and QUIET arguments, and set YAML_CPP_FOUND if found.
 include(FindPackageHandleStandardArgs)
@@ -16,12 +18,18 @@ find_package_handle_standard_args(
     DEFAULT_MSG
     YAML_CPP_LIBRARY YAML_CPP_INCLUDE_DIR)
 
-#  TODO: Infer the version string from the YAMLCPP installation instead.
-set(YAML_CPP_VERSION "0.0.0" CACHE STRING "The version of the YAMLCPP Library")
-
 if(YAML_CPP_FOUND)
     set(YAML_CPP_LIBRARIES ${YAML_CPP_LIBRARY})
     set(YAML_CPP_INCLUDE_DIRS ${YAML_CPP_INCLUDE_DIR})
+    if (PC_YAML_CPP_VERSION)
+    	set(YAML_CPP_VERSION ${PC_YAML_CPP_VERSION})
+    else()
+		#  TODO: Infer the version string from the YAMLCPP installation instead.
+		set(YAML_CPP_VERSION "0.0.0" CACHE STRING "The version of the YAML_CPP librar.")
+        if(YAML_CPP_VERSION STREQUAL "0.0.0")
+        	message(FATAL_ERROR "Could not determine the version of the YAML_CPP library automatically. Please set YAML_CPP_VERSION manually.")
+        endif()
+    endif()
 endif()
 
 mark_as_advanced(YAML_CPP_LIBRARY YAML_CPP_INCLUDE_DIR)
