6376789c4fd1b6838b706d3b3a2a2ac7a9c93273
Mark Visser <mvisser@mokkostudio.com>
2015-08-06 14:31:24 -0400

    added args/kwargs support to Python constructors for MatrixTransform, AllocationTransform, CDLTransform and DisplayTransform

--- src/pyglue/PyDisplayTransform.cpp
+++ src/pyglue/PyDisplayTransform.cpp
@@ -165,10 +165,25 @@ OCIO_NAMESPACE_ENTER
         ///////////////////////////////////////////////////////////////////////
         ///
         
-        int PyOCIO_DisplayTransform_init(PyOCIO_Transform * self, PyObject * /*args*/, PyObject * /*kwds*/)
+        int PyOCIO_DisplayTransform_init(PyOCIO_Transform * self, PyObject * args, PyObject * kwds)
         {
             OCIO_PYTRY_ENTER()
-            return BuildPyTransformObject<DisplayTransformRcPtr>(self, DisplayTransform::Create());
+            DisplayTransformRcPtr ptr = DisplayTransform::Create();
+            int ret = BuildPyTransformObject<DisplayTransformRcPtr>(self, ptr);
+            char* inputColorSpaceName = NULL;
+            char* display = NULL;
+            char* view = NULL;
+            char* direction = NULL;
+            static const char *kwlist[] = { "inputColorSpaceName",
+                "display", "view", "direction", NULL };
+            if(!PyArg_ParseTupleAndKeywords(args, kwds, "|ssss",
+                const_cast<char **>(kwlist),
+                &inputColorSpaceName, &display, &view, &direction)) return -1;
+            if(inputColorSpaceName) ptr->setInputColorSpaceName(inputColorSpaceName);
+            if(display) ptr->setDisplay(display);
+            if(view) ptr->setView(view);
+            if(direction) ptr->setDirection(TransformDirectionFromString(direction));
+            return ret;
             OCIO_PYTRY_EXIT(-1)
         }
         
