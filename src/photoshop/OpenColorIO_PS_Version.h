4005b767dfbfa2d9a3deec08f0cee27d5dfb4a47
Brendan Bolles <brendan@fnordware.com>
2018-03-14 17:02:25 -0700

    Update build date

--- src/photoshop/OpenColorIO_PS_Version.h
+++ src/photoshop/OpenColorIO_PS_Version.h
@@ -34,7 +34,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define OpenColorIO_PS_Minor_Version 1
 #define OpenColorIO_PS_Version_String "1.1.0"
 #define OpenColorIO_PS_Build_Date __DATE__
-#define OpenColorIO_PS_Build_Date_Manual "5 February 2018"
+#define OpenColorIO_PS_Build_Date_Manual "14 March 2018"
 #define OpenColorIO_PS_Build_Complete_Manual "v1.1.0 - " OpenColorIO_PS_Build_Date
 #define OpenColorIO_PS_Copyright_Year "2018"
 #define OpenColorIO_PS_Build_Year "2018"
