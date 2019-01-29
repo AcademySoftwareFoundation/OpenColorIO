84b3f584fa932af4c3d17fda04b8855ccaea73d7
Bernard Lefebvre <37628108+BernardLefebvre@users.noreply.github.com>
2019-01-29 16:23:50 -0500

    Fix include file case

--- src/OpenColorIO/fileformats/xmlutils/XMLReaderUtils.cpp
+++ src/OpenColorIO/fileformats/xmlutils/XMLReaderUtils.cpp
@@ -28,7 +28,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <algorithm>
 
-#include "fileformats/xmlutils/XmlReaderUtils.h"
+#include "fileformats/xmlutils/XMLReaderUtils.h"
 
 OCIO_NAMESPACE_ENTER
 {
