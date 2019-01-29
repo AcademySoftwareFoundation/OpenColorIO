adb82ee870722578cdf547045e8cae9fbe88e85e
Bernard Lefebvre <37628108+BernardLefebvre@users.noreply.github.com>
2019-01-29 16:29:51 -0500

    Fix include file case

--- src/OpenColorIO/ops/IndexMapping.cpp
+++ src/OpenColorIO/ops/IndexMapping.cpp
@@ -28,7 +28,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <sstream>
 
-#include "Ops/IndexMapping.h"
+#include "ops/IndexMapping.h"
 
 OCIO_NAMESPACE_ENTER
 {
