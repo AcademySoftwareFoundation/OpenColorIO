9ae285c6130d376e8c504c8862e822ec22d32d20
Michael Dolan <mdolan@imageworks.com>
2019-06-07 14:14:09 -0700

    Enable quiet install for Python msi

--- share/ci/scripts/install_win2012r2.ps1
+++ share/ci/scripts/install_win2012r2.ps1
@@ -1,10 +1,10 @@
-# Upgrade Python
+# Install Python
 Invoke-WebRequest https://www.python.org/ftp/python/2.7.16/python-2.7.16.amd64.msi -OutFile C:\python-2.7.16.amd64.msi
-msiexec /i C:\python-2.7.16.amd64.msi ALLUSERS=1 ADDLOCAL=ALL TARGETDIR=C:\_python27 /log
+msiexec /i C:\python-2.7.16.amd64.msi /quiet ALLUSERS=1 ADDLOCAL=ALL TARGETDIR=C:\_python27
 Write-Host "##vso[task.prependpath]C:\_python27"
 Write-Host "##vso[task.prependpath]C:\_python27\Scripts"
 
-# Upgrade CMake
+# Install CMake
 Invoke-WebRequest https://cmake.org/files/v3.14/cmake-3.14.4-win64-x64.zip -OutFile C:\cmake-3.14.4-win64-x64.zip
 Expand-Archive C:\cmake-3.14.4-win64-x64.zip -DestinationPath C:\
 Rename-Item -Path C:\cmake-3.14.4-win64-x64 -NewName C:\_cmake
