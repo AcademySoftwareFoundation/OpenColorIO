190f652af24b918ee3b7f5cc4c714e71634ca147
michdolan <michdolan@gmail.com>
2019-07-25 02:08:53 -0700

    Guard Nuke doc build, Update Dockerfile and scripts

--- share/ci/scripts/linux/install_cmake.sh
+++ share/ci/scripts/linux/install_cmake.sh
@@ -5,5 +5,5 @@ set -ex
 CMAKE_VERSION="$1"
 
 curl --location "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-Linux-x86_64.sh" -o "cmake.sh" &&\
-sudo sh cmake.sh --skip-license
+sudo sh cmake.sh --skip-license --prefix=/usr/local
 rm cmake.sh
