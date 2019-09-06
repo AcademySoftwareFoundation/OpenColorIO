1c624651b7ef414353c8ab790595b1db89f59616
Michael Dolan <michdolan@gmail.com>
2019-09-06 12:43:59 -0700

    CI branch cleanup RB-1.1 (#798)
    * Limit AppVeyor to RB- branches
    
    * Limit Travis to RB- branches
    
    * Fix self-assign-field warning
    
    * Remove unused declaration, add CXX_STANDARD warning

--- src/core/Config.cpp
+++ src/core/Config.cpp
@@ -330,7 +330,7 @@ OCIO_NAMESPACE_ENTER
                 sanitytext_ = rhs.sanitytext_;
                 
                 cacheids_ = rhs.cacheids_;
-                cacheidnocontext_ = cacheidnocontext_;
+                cacheidnocontext_ = rhs.cacheidnocontext_;
             }
             return *this;
         }
