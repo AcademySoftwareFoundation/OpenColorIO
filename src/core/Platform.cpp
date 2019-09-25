c8d8f428ba5d212758dbbe13647b3dff279d268e
Patrick Hodoul <23659034+hodoulp@users.noreply.github.com>
2019-09-25 17:11:23 -0400

    Adsk Contrib - Port a CDL Transform fix from master to OCIOv1 (#835)
    * Adsk Contrib - Port a CDL Transform fix from master to OCIOv1
    
    * Fix Linux build break
    
    * Fix Linux build break, part II

--- src/core/Platform.cpp
+++ src/core/Platform.cpp
@@ -3,9 +3,13 @@
 //
 //
 
+#include <sstream>
+#include <cstdlib>
+
 #include <OpenColorIO/OpenColorIO.h>
 
 #include "Platform.h"
+
 
 OCIO_NAMESPACE_ENTER
 {
@@ -52,6 +56,34 @@ OCIO_NAMESPACE_ENTER
 #else
             ::setenv(name, value.c_str(), 1);
 #endif
+        }
+
+
+        void createTempFilename(std::string & filename, const std::string & filenameExt)
+        {
+            // Note: Because of security issue, tmpnam could not be used.
+
+#ifdef _WIN32
+
+            char tmpFilename[L_tmpnam];
+            if(tmpnam_s(tmpFilename))
+            {
+                throw Exception("Could not create a temporary file.");
+            }
+
+            filename = tmpFilename;
+
+#else
+
+            std::stringstream ss;
+            ss << "/tmp/ocio";
+            ss << std::rand();
+
+            filename = ss.str();
+
+#endif
+
+            filename += filenameExt;
         }
 
     }//namespace platform
@@ -121,6 +153,19 @@ OIIO_ADD_TEST(Platform, setenv)
         OCIO::Platform::getenv("MY_DUMMY_ENV", env);
         OIIO_CHECK_ASSERT(env.empty());
     }
+}
+
+OIIO_ADD_TEST(Platform, createTempFilename)
+{
+    std::string f1, f2;
+
+    OIIO_CHECK_NO_THROW(OCIO::Platform::createTempFilename(f1, ""));
+    OIIO_CHECK_NO_THROW(OCIO::Platform::createTempFilename(f2, ""));
+    OIIO_CHECK_ASSERT(f1!=f2);
+
+    OIIO_CHECK_NO_THROW(OCIO::Platform::createTempFilename(f1, ".ctf"));
+    OIIO_CHECK_NO_THROW(OCIO::Platform::createTempFilename(f2, ".ctf"));
+    OIIO_CHECK_ASSERT(f1!=f2);
 }
 
 #endif // OCIO_UNIT_TEST
