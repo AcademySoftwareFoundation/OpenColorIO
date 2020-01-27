027db733a110ef14f93aab40480c2690e37fd3f6
Patrick Hodoul <Patrick.Hodoul@autodesk.com>
2020-01-27 11:08:37 -0500

    Rename the directory for the string utils
    Signed-off-by: Patrick Hodoul <Patrick.Hodoul@autodesk.com>

--- src/utils/stringutils/StringUtils.h
+++ src/utils/stringutils/StringUtils.h
@@ -16,40 +16,35 @@ namespace StringUtils
 inline std::string Lower(std::string str)
 {
     std::transform(str.begin(), str.end(), str.begin(),
-                   [](unsigned char c){ return std::tolower(c); }
-                  );
+                   [](unsigned char c) { return std::tolower(c); });
     return str;
 }
 
 inline std::string Upper(std::string str)
 {
     std::transform(str.begin(), str.end(), str.begin(),
-                   [](unsigned char c){ return std::toupper(c); }
-                  );
+                   [](unsigned char c) { return std::toupper(c); });
     return str;
 }
 
 inline std::string LeftTrim(std::string str)
 {
-    const auto it = std::find_if(str.begin(), str.end(),
-                                 [](char ch){ return !std::isspace(ch); }
-                                );
+    const auto it = std::find_if(str.begin(), str.end(), [](char ch) { return !std::isspace(ch); });
     str.erase(str.begin(), it);
     return str;
 }
 
 inline std::string RightTrim(std::string str)
 {
-    const auto it = std::find_if(str.rbegin(), str.rend(),
-                                 [](char ch){ return !std::isspace(ch); }
-                                );
+    const auto it =
+        std::find_if(str.rbegin(), str.rend(), [](char ch) { return !std::isspace(ch); });
     str.erase(it.base(), str.end());
     return str;
 }
 
 inline std::string Trim(std::string str)
 {
-   return LeftTrim(RightTrim(str));
+    return LeftTrim(RightTrim(str));
 }
 
 inline void Split(const std::string & str, std::vector<std::string> & results, char separator)
@@ -69,18 +64,15 @@ inline void SplitByLines(const std::string & str, std::vector<std::string> & res
 
 inline bool EndsWith(const std::string & str, const std::string & suffix)
 {
-    return str.size() >= suffix.size()
-        && 0 == str.compare(str.size()-suffix.size(), suffix.size(), suffix);
+    return str.size() >= suffix.size() &&
+           0 == str.compare(str.size() - suffix.size(), suffix.size(), suffix);
 }
 
 inline bool StartsWith(const std::string & str, const std::string & prefix)
 {
-    return str.size() >= prefix.size()
-        && 0 == str.compare(0, prefix.size(), prefix);
+    return str.size() >= prefix.size() && 0 == str.compare(0, prefix.size(), prefix);
 }
-
 
 } // namespace StringUtils
 
-
-#endif  // INCLUDED_STRINGUTILS_H
+#endif // INCLUDED_STRINGUTILS_H
