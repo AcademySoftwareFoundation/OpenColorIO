ba52937fd5c39fde2f64d3e149d70413b45baf1e
Patrick Hodoul <patrickhodoul@gmail.com>
2020-05-19 10:04:01 -0400

    Adsk Contrib - Optimize the GPU texture size for 1D LUTs (#1013)
    Signed-off-by: Patrick Hodoul <Patrick.Hodoul@autodesk.com>
    
    Co-authored-by: doug-walker <43830961+doug-walker@users.noreply.github.com>

--- tests/cpu/ops/lut1d/Lut1DOpGPU_tests.cpp
+++ tests/cpu/ops/lut1d/Lut1DOpGPU_tests.cpp
@@ -14,7 +14,7 @@ namespace OCIO = OCIO_NAMESPACE;
 
 OCIO_ADD_TEST(Lut1DOp, pad_lut_one_dimension)
 {
-    const unsigned width = 6;
+    static constexpr unsigned width = 6;
 
     // Create a channel multi row and smaller than the expected texture size.
 
@@ -25,23 +25,25 @@ OCIO_ADD_TEST(Lut1DOp, pad_lut_one_dimension)
 
     for (unsigned idx = 0; idx<channel.size() / 3; ++idx)
     {
-        channel[3*idx] = float(idx);
-        channel[3*idx + 1] = float(idx) + 0.1f;
-        channel[3*idx + 2] = float(idx) + 0.2f;
+        channel[3 * idx + 0] = float(idx);
+        channel[3 * idx + 1] = float(idx) + 0.1f;
+        channel[3 * idx + 2] = float(idx) + 0.2f;
     }
 
     // Pad the texture values.
 
     std::vector<float> chn;
-    OCIO_CHECK_NO_THROW(OCIO::PadLutChannels(width, 1, channel, chn));
+    OCIO_CHECK_NO_THROW(OCIO::CreatePaddedLutChannels(width, 1, channel, chn));
 
     // Check the values.
 
-    const float res[18] = { 0.0f, 0.1f, 0.2f, 1.0f, 1.1f, 1.2f,
-                            2.0f, 2.1f, 2.2f, 3.0f, 3.1f, 3.2f,
-                            3.0f, 3.1f, 3.2f, 3.0f, 3.1f, 3.2f };
+    static constexpr float res[18] = {
+        0.0f, 0.1f, 0.2f, 1.0f, 1.1f, 1.2f,
+        2.0f, 2.1f, 2.2f, 3.0f, 3.1f, 3.2f,
+        3.0f, 3.1f, 3.2f, 3.0f, 3.1f, 3.2f };
+
     OCIO_CHECK_EQUAL(chn.size(), 18);
-    for (unsigned idx = 0; idx<chn.size(); ++idx)
+    for (unsigned idx = 0; idx < chn.size(); ++idx)
     {
         OCIO_CHECK_EQUAL(chn[idx], res[idx]);
     }
@@ -49,28 +51,29 @@ OCIO_ADD_TEST(Lut1DOp, pad_lut_one_dimension)
 
 OCIO_ADD_TEST(Lut1DOp, pad_lut_two_dimension_1)
 {
-    const unsigned width = 4;
-    const unsigned height = 3;
+    static constexpr unsigned width  = 4;
+    static constexpr unsigned height = 3;
 
     std::vector<float> channel;
     channel.resize((height * width - 4) * 3);
 
-    for (unsigned idx = 0; idx<channel.size() / 3; ++idx)
+    for (unsigned idx = 0; idx < (channel.size() / 3); ++idx)
     {
-        channel[3 * idx] = float(idx);
+        channel[3 * idx + 0] = float(idx);
         channel[3 * idx + 1] = float(idx) + 0.1f;
         channel[3 * idx + 2] = float(idx) + 0.2f;
     }
 
     std::vector<float> chn;
-    OCIO_CHECK_NO_THROW(OCIO::PadLutChannels(width, height, channel, chn));
+    OCIO_CHECK_NO_THROW(OCIO::CreatePaddedLutChannels(width, height, channel, chn));
 
-    const float res[] = {
+    static constexpr float res[] = {
         0.0f, 0.1f, 0.2f, 1.0f, 1.1f, 1.2f, 2.0f, 2.1f, 2.2f, 3.0f, 3.1f, 3.2f,
         3.0f, 3.1f, 3.2f, 4.0f, 4.1f, 4.2f, 5.0f, 5.1f, 5.2f, 6.0f, 6.1f, 6.2f, 
         6.0f, 6.1f, 6.2f, 7.0f, 7.1f, 7.2f, 7.0f, 7.1f, 7.2f, 7.0f, 7.1f, 7.2f };
+
     OCIO_CHECK_EQUAL(chn.size(), 36);
-    for (unsigned idx = 0; idx<chn.size(); ++idx)
+    for (unsigned idx = 0; idx < chn.size(); ++idx)
     {
         OCIO_CHECK_EQUAL(chn[idx], res[idx]);
     }
@@ -78,35 +81,56 @@ OCIO_ADD_TEST(Lut1DOp, pad_lut_two_dimension_1)
 
 OCIO_ADD_TEST(Lut1DOp, pad_lut_two_dimension_2)
 {
-    const unsigned width = 4;
-    const unsigned height = 3;
+    // Requested GPU texture dimensions.
+    static constexpr unsigned width  = 4;
+    static constexpr unsigned height = 3;
 
-    std::vector<float> channel;
-    channel.resize((height * width - 3) * 3);
+    // Internally, all LUTs have three channels (R, G & B).
+    static const std::vector<float> lutValues {
+        0.0f, 0.1f, 0.2f,   1.0f, 1.1f, 1.2f,   2.0f, 2.1f, 2.2f,
+        3.0f, 3.1f, 3.2f,   4.0f, 4.1f, 4.2f,   5.0f, 5.1f, 5.2f, 
+        6.0f, 6.1f, 6.2f,   7.0f, 7.1f, 7.2f,   8.0f, 8.1f, 8.2f };
 
-    for (unsigned idx = 0; idx<channel.size() / 3; ++idx)
     {
-        channel[3 * idx] = float(idx);
-        channel[3 * idx + 1] = float(idx) + 0.1f;
-        channel[3 * idx + 2] = float(idx) + 0.2f;
+        // Create the padded buffer used by a GPU texture to perform the right
+        // linear interpolation even for the last (on each row) texel value.
+        std::vector<float> chn;
+        OCIO_CHECK_NO_THROW(OCIO::CreatePaddedLutChannels(width, height, lutValues, chn));
+
+        // Here is the expected buffer for the 2D Texture padded to width & height for
+        // the three channels (R, G, B).
+        static constexpr float res[width * height * 3] = {
+            0.0f, 0.1f, 0.2f,  1.0f, 1.1f, 1.2f,  2.0f, 2.1f, 2.2f,  3.0f, 3.1f, 3.2f,
+            3.0f, 3.1f, 3.2f,  4.0f, 4.1f, 4.2f,  5.0f, 5.1f, 5.2f,  6.0f, 6.1f, 6.2f,
+            6.0f, 6.1f, 6.2f,  7.0f, 7.1f, 7.2f,  8.0f, 8.1f, 8.2f,  8.0f, 8.1f, 8.2f };
+
+        OCIO_CHECK_EQUAL(chn.size(), (width * height * 3));
+
+        for (unsigned idx = 0; idx < chn.size(); ++idx)
+        {
+            OCIO_CHECK_EQUAL(chn[idx], res[idx]);
+        }
     }
 
-    // Special case where size%(width-1) = 0
-    std::vector<float> chn;
-    OCIO_CHECK_NO_THROW(OCIO::PadLutChannels(width, height, channel, chn));
-
-    // Check the values
-
-    const float res[] = {
-        0.0f, 0.1f, 0.2f, 1.0f, 1.1f, 1.2f, 2.0f, 2.1f, 2.2f, 3.0f, 3.1f, 3.2f,
-        3.0f, 3.1f, 3.2f, 4.0f, 4.1f, 4.2f, 5.0f, 5.1f, 5.2f, 6.0f, 6.1f, 6.2f,
-        6.0f, 6.1f, 6.2f, 7.0f, 7.1f, 7.2f, 8.0f, 8.1f, 8.2f, 8.0f, 8.1f, 8.2f };
-
-    OCIO_CHECK_EQUAL(chn.size(), 36);
-
-    for (unsigned idx = 0; idx<chn.size(); ++idx)
     {
-        OCIO_CHECK_EQUAL(chn[idx], res[idx]);
+        // Test as all channels were identical i.e. only the RED one is used & padded.
+
+        std::vector<float> paddedChannel;
+        OCIO_CHECK_NO_THROW(OCIO::CreatePaddedRedChannel(width, height, lutValues, paddedChannel));
+
+        // Here is the expected buffer for the 2D Texture padded to width & height for
+        // the Red channel only i.e. no G & B channels.
+        static constexpr float res[width * height * 1] = {
+            0.0f,  1.0f,  2.0f,  3.0f,
+            3.0f,  4.0f,  5.0f,  6.0f,
+            6.0f,  7.0f,  8.0f,  8.0f };
+
+        OCIO_REQUIRE_EQUAL(paddedChannel.size(), (width * height * 1));
+
+        for (unsigned idx = 0; idx < paddedChannel.size(); ++idx)
+        {
+            OCIO_CHECK_EQUAL(paddedChannel[idx], res[idx]);
+        }
     }
 }
 
