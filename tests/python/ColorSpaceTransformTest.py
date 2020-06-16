f99932c2ddd73f6af8e90df78e77906d4cf919d3
Mei Chu <meimchu@gmail.com>
2020-06-15 19:00:11 -0700

    Updated Transforms python unit tests first by breaking them down. Starting with: - AllocatioNTransformTest - ColorSpaceTransformTest
    Signed-off-by: Mei Chu <meimchu@gmail.com>

---
+++ tests/python/ColorSpaceTransformTest.py
@@ -0,0 +1,113 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright Contributors to the OpenColorIO Project.
+
+import unittest
+
+import PyOpenColorIO as OCIO
+from UnitTestUtils import TEST_NAMES
+
+
+class ColorSpaceTransformTest(unittest.TestCase):
+    TEST_DIRECTION = OCIO.TRANSFORM_DIR_FORWARD
+    TEST_SRC = 'foo'
+    TEST_DST = 'bar'
+
+    def setUp(self):
+        self.cs_tr = OCIO.ColorSpaceTransform()
+
+    def tearDown(self):
+        self.cs_tr = None
+
+    def test_colorspace_src(self):
+        """
+        Test the setSrc() and getSrc() methods.
+        """
+
+        # Default initialized src value is ""
+        self.assertEqual(self.cs_tr.getSrc(), '')
+
+        for name in TEST_NAMES:
+            self.cs_tr.setSrc(name)
+            self.assertEqual(name, self.cs_tr.getSrc())
+
+        # Wrong type tests.
+        for invalid in (None, 1):
+            with self.assertRaises(TypeError):
+                self.cs_tr.setSrc(invalid)
+
+    def test_colorspace_dst(self):
+        """
+        Test the setDst() and getDst() methods.
+        """
+
+        # Default initialized src value is ""
+        self.assertEqual(self.cs_tr.getDst(), '')
+
+        for name in TEST_NAMES:
+            self.cs_tr.setDst(name)
+            self.assertEqual(name, self.cs_tr.getDst())
+
+        # Wrong type tests.
+        for invalid in (None, 1):
+            with self.assertRaises(TypeError):
+                self.cs_tr.setDst(invalid)
+
+    def test_direction(self):
+        """
+        Test the setDirection() and getDirection() methods.
+        """
+
+        # Default initialized direction is forward.
+        self.assertEqual(self.cs_tr.getDirection(),
+                         OCIO.TRANSFORM_DIR_FORWARD)
+
+        for direction in OCIO.TransformDirection.__members__.values():
+            # Setting the unknown direction preserves the current direction.
+            if direction != OCIO.TRANSFORM_DIR_UNKNOWN:
+                self.cs_tr.setDirection(direction)
+                self.assertEqual(self.cs_tr.getDirection(), direction)
+
+        # Wrong type tests.
+        for invalid in (None, 1):
+            with self.assertRaises(TypeError):
+                self.cs_tr.setDirection(invalid)
+
+    def test_constructor_with_positional(self):
+        """
+        Test ColorSpaceTransform constructor with keywords and validate its values.
+        """
+
+        # With keywords in their proper order.
+        cs_tr = OCIO.ColorSpaceTransform(src=self.TEST_SRC,
+                                         dst=self.TEST_DST,
+                                         dir=self.TEST_DIRECTION)
+
+        self.assertEqual(cs_tr.getDirection(), self.TEST_DIRECTION)
+        self.assertEqual(cs_tr.getSrc(), self.TEST_SRC)
+        self.assertEqual(cs_tr.getDst(), self.TEST_DST)
+
+        # With keywords not in their proper order.
+        cs_tr2 = OCIO.ColorSpaceTransform(dir=self.TEST_DIRECTION,
+                                          src=self.TEST_SRC,
+                                          dst=self.TEST_DST)
+
+        self.assertEqual(cs_tr2.getDirection(), self.TEST_DIRECTION)
+        self.assertEqual(cs_tr2.getSrc(), self.TEST_SRC)
+        self.assertEqual(cs_tr2.getDst(), self.TEST_DST)
+
+    def test_constructor_without_positional(self):
+        """
+        Test ColorSpaceTransform constructor without keywords and validate its values.
+        """
+
+        cs_tr = OCIO.ColorSpaceTransform(self.TEST_SRC,
+                                         self.TEST_DST,
+                                         self.TEST_DIRECTION)
+
+        self.assertEqual(cs_tr.getDirection(), self.TEST_DIRECTION)
+        self.assertEqual(cs_tr.getSrc(), self.TEST_SRC)
+        self.assertEqual(cs_tr.getDst(), self.TEST_DST)
+
+# TODO: Questions to ask in PR.
+# setSrc() and setDst() wrong type tests will crash python
+# <ColorSpaceTransform direction=forward, src=, dst=> is not the right order and also dir vs direction
--- tests/python/OpenColorIOTestSuite.py
+++ tests/python/OpenColorIOTestSuite.py
@@ -33,12 +33,14 @@ import PyOpenColorIO as OCIO
 import ColorSpaceTest
 import BuiltinTransformRegistryTest
 import BuiltinTransformTest
+import LookTest
 import CDLTransformTest
+import AllocationTransformTest
+import ColorSpaceTransformTest
 #from MainTest import *
 #from ConstantsTest import *
 #from ConfigTest import *
 #from ContextTest import *
-#from LookTest import *
 #from GpuShaderDescTest import *
 #from Baker import *
 #from TransformsTest import *
@@ -58,13 +60,15 @@ def suite():
     suite.addTest(loader.loadTestsFromModule(ColorSpaceTest))
     suite.addTest(loader.loadTestsFromModule(BuiltinTransformRegistryTest))
     suite.addTest(loader.loadTestsFromModule(BuiltinTransformTest))
+    suite.addTest(loader.loadTestsFromModule(LookTest))
     suite.addTest(loader.loadTestsFromModule(CDLTransformTest))
+    suite.addTest(loader.loadTestsFromModule(AllocationTransformTest))
+    suite.addTest(loader.loadTestsFromModule(ColorSpaceTransformTest))
     #suite.addTest(MainTest("test_interface"))
     #suite.addTest(ConstantsTest("test_interface"))
     #suite.addTest(ConfigTest("test_interface"))
     #suite.addTest(ConfigTest("test_is_editable"))
     #suite.addTest(ContextTest("test_interface"))
-    #suite.addTest(LookTest("test_interface"))
     #suite.addTest(RangeTransformTest("test_interface"))
     #suite.addTest(RangeTransformTest("test_equality"))
     #suite.addTest(RangeTransformTest("test_validation"))
