83898cbc84e6cfd91e556c2fab4ddf2ec3e82707
Patrick Hodoul <Patrick.Hodoul@autodesk.com>
2020-09-11 08:32:11 -0400

    Adsk Contrib - Add 2021 VFX Reference Platform requirements
    Signed-off-by: Patrick Hodoul <Patrick.Hodoul@autodesk.com>

--- share/cmake/modules/FindExtPackages.cmake
+++ share/cmake/modules/FindExtPackages.cmake
@@ -15,8 +15,11 @@
 # when a package has previously been installed to ext/dist. Set these variables
 # to OFF during cmake configuration to enable package registry use.
 
-set(CMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY ON CACHE BOOL "Disable CMake User Package Registry when finding packages")
-set(CMAKE_FIND_PACKAGE_NO_SYSTEM_PACKAGE_REGISTRY ON CACHE BOOL "Disable CMake System Package Registry when finding packages")
+set(CMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY ON CACHE BOOL
+    "Disable CMake User Package Registry when finding packages")
+
+set(CMAKE_FIND_PACKAGE_NO_SYSTEM_PACKAGE_REGISTRY ON CACHE BOOL
+    "Disable CMake System Package Registry when finding packages")
 
 ###############################################################################
 ### Packages and versions ###
