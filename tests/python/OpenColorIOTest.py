5401b6213c3a5fc89e1f3518049f3e8bff47c7dc
Frankie Eder <frankaeder@gmail.com>
2021-01-11 22:15:20 -0800

    Committing draft of tests.
    Signed-off-by: Frankie Eder <frankaeder@gmail.com>

--- tests/python/OpenColorIOTest.py
+++ tests/python/OpenColorIOTest.py
@@ -5,6 +5,7 @@
 import unittest
 import os
 import sys
+import inspect
 
 import PyOpenColorIO as OCIO
 
@@ -26,3 +27,28 @@ class OpenColorIOTest(unittest.TestCase):
         OCIO.SetEnvVariable(value='TOTO', name='MY_ENVAR')
         self.assertTrue(OCIO.IsEnvVariablePresent(name='MY_ENVAR'))
         self.assertEqual(OCIO.GetEnvVariable(name='MY_ENVAR'), 'TOTO')
+
+    def test_bindings_transforms(self):
+        """
+        Tests polymorphism issue where transforms are cast as parent class when using GroupTransforms.
+        Flagged in https://github.com/AcademySoftwareFoundation/OpenColorIO/issues/1211
+        """
+        allTransformsAsGroup = OCIO.GroupTransform()
+        for n, c in inspect.getmembers(OCIO): # Search for transform types to handle future transforms
+            if hasattr(c, 'getTransformType'):
+                try:
+                    allTransformsAsGroup.appendTransform(c())
+                except TypeError as e:
+                    # Catch and filter only the Exception caused by trying to instantiate
+                    # parent OCIO.Transform class
+                    self.assertEqual(str(e), 'PyOpenColorIO.Transform: No constructor defined!')
+        for transform in allTransformsAsGroup:
+            # Ensure none have been cast as parent transform
+            self.assertNotEqual(
+                type(transform),
+                OCIO.Transform,
+                """Transform has unintentionally been cast as parent class!
+                transform.getTransformType(): {0}
+                type(transform): {1}
+                """.format(transform.getTransformType(), type(transform))
+            )
