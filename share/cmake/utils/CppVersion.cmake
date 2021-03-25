fa061c93fad1ff49b6f93f918ef57ee8775335fd
Patrick Hodoul <Patrick.Hodoul@autodesk.com>
2021-03-25 18:05:46 -0400

    Isolate the C++ version detection from warnings related to custom CXX flags
    Signed-off-by: Patrick Hodoul <Patrick.Hodoul@autodesk.com>

--- share/cmake/utils/CppVersion.cmake
+++ share/cmake/utils/CppVersion.cmake
@@ -20,41 +20,53 @@ set_property(CACHE CMAKE_CXX_STANDARD PROPERTY STRINGS "${SUPPORTED_CXX_STANDARD
 
 include(CheckCXXCompilerFlag)
 
+# As CheckCXXCompilerFlag implicitly uses CMAKE_CXX_FLAGS some custom flags could trigger unrelated
+# warnings causing a detection failure. So, the code disables all warnings to focus on the C++
+# version detection.
+if(USE_MSVC)
+    set(CUSTOM_CXX_FLAGS "/w")
+else()
+    set(CUSTOM_CXX_FLAGS "-w")
+endif()
+
 if(${CMAKE_CXX_STANDARD} EQUAL 11)
     if(USE_MSVC)
-        CHECK_CXX_COMPILER_FLAG("-std:c++11" COMPILER_SUPPORTS_CXX11)
+        CHECK_CXX_COMPILER_FLAG("${CUSTOM_CXX_FLAGS} -std:c++11" COMPILER_SUPPORTS_CXX11)
     else()
-        CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
+        CHECK_CXX_COMPILER_FLAG("${CUSTOM_CXX_FLAGS} -std=c++11" COMPILER_SUPPORTS_CXX11)
     endif()
 
     if(NOT COMPILER_SUPPORTS_CXX11)
-        message(STATUS "The compiler ${CMAKE_CXX_COMPILER_ID} has no C++11 only support. Use C++14.")
+        message(STATUS 
+            "The compiler ${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION} has no C++11 only support. Use C++14.")
         set(CMAKE_CXX_STANDARD 14)
     endif()
 endif()
 
 if(${CMAKE_CXX_STANDARD} EQUAL 14)
     if(USE_MSVC)
-        CHECK_CXX_COMPILER_FLAG("-std:c++14" COMPILER_SUPPORTS_CXX14)
+        CHECK_CXX_COMPILER_FLAG("${CUSTOM_CXX_FLAGS} -std:c++14" COMPILER_SUPPORTS_CXX14)
     else()
-        CHECK_CXX_COMPILER_FLAG("-std=c++14" COMPILER_SUPPORTS_CXX14)
+        CHECK_CXX_COMPILER_FLAG("${CUSTOM_CXX_FLAGS} -std=c++14" COMPILER_SUPPORTS_CXX14)
     endif()
 
     if(NOT COMPILER_SUPPORTS_CXX14)
-        message(STATUS "The compiler ${CMAKE_CXX_COMPILER_ID} has no C++14 only support. Use C++17.")
+        message(STATUS 
+            "The compiler ${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION} has no C++14 only support. Use C++17.")
         set(CMAKE_CXX_STANDARD 17)
     endif()
 endif()
 
 if(${CMAKE_CXX_STANDARD} EQUAL 17)
     if(USE_MSVC)
-        CHECK_CXX_COMPILER_FLAG("-std:c++17" COMPILER_SUPPORTS_CXX17)
+        CHECK_CXX_COMPILER_FLAG("${CUSTOM_CXX_FLAGS} -std:c++17" COMPILER_SUPPORTS_CXX17)
     else()
-        CHECK_CXX_COMPILER_FLAG("-std=c++17" COMPILER_SUPPORTS_CXX17)
+        CHECK_CXX_COMPILER_FLAG("${CUSTOM_CXX_FLAGS} -std=c++17" COMPILER_SUPPORTS_CXX17)
     endif()
 
     if(NOT COMPILER_SUPPORTS_CXX17)
-        message(FATAL_ERROR "The compiler ${CMAKE_CXX_COMPILER_ID} has no C++17 support.")
+        message(FATAL_ERROR 
+            "The compiler ${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION} has no C++17 support.")
     endif()
 endif()
 
