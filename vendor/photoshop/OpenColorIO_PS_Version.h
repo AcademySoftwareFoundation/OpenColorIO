61746025b3270332a28c4530eb417660584cc116
Brendan Bolles <brendan@fnordware.com>
2021-05-15 09:56:20 -0700

    Back version up
    Signed-off-by: Brendan Bolles <brendan@fnordware.com>

--- vendor/photoshop/OpenColorIO_PS_Version.h
+++ vendor/photoshop/OpenColorIO_PS_Version.h
@@ -6,11 +6,11 @@
 
 
 #define OpenColorIO_PS_Major_Version 2
-#define OpenColorIO_PS_Minor_Version 1
-#define OpenColorIO_PS_Version_String "2.1.0"
+#define OpenColorIO_PS_Minor_Version 0
+#define OpenColorIO_PS_Version_String "2.0.1"
 #define OpenColorIO_PS_Build_Date __DATE__
-#define OpenColorIO_PS_Build_Date_Manual "14 April 2021"
-#define OpenColorIO_PS_Build_Complete_Manual "v2.1.0 - " OpenColorIO_PS_Build_Date
+#define OpenColorIO_PS_Build_Date_Manual "15 May 2021"
+#define OpenColorIO_PS_Build_Complete_Manual "v2.0.1 - " OpenColorIO_PS_Build_Date
 #define OpenColorIO_PS_Copyright_Year "2021"
 #define OpenColorIO_PS_Build_Year "2021"
 
