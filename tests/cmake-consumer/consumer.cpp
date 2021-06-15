49d6735b91fa3c870704adb6925206105f459b68
Rémi Achard <remiachard@gmail.com>
2021-06-15 22:03:19 +0100

    Test CMake config export consuming
    Signed-off-by: Rémi Achard <remiachard@gmail.com>

---
+++ tests/cmake-consumer/consumer.cpp
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright Contributors to the OpenColorIO Project.
+
+
+// Check that OCIO library can be consumed by a cmake build
+
+#include <cassert>
+#include <iostream>
+
+#include <OpenColorIO/OpenColorIO.h>
+
+namespace OCIO = OCIO_NAMESPACE;
+
+
+int main(int argc, char** argv)
+{
+    auto reg = OCIO::BuiltinTransformRegistry::Get();
+    assert(reg->getNumBuiltins() > 0);
+    return 0;
+}
