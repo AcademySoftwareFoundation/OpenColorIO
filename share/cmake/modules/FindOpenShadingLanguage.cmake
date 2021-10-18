4b982ae68474724b9ba3e63583ed5968d520f410
Patrick Hodoul <Patrick.Hodoul@autodesk.com>
2021-10-18 10:21:01 -0400

    Improve OSL library find
    Signed-off-by: Patrick Hodoul <Patrick.Hodoul@autodesk.com>

--- share/cmake/modules/FindOpenShadingLanguage.cmake
+++ share/cmake/modules/FindOpenShadingLanguage.cmake
@@ -18,7 +18,7 @@ endif()
 
 if(NOT DEFINED OSL_ROOT)
 
-    find_package(OSL ${OpenShaderLanguage_VERSION} CONFIG)
+    find_package(OSL ${OpenShadingLanguage_VERSION} CONFIG)
 
     # TODO: No variable to have the share directory?
 
@@ -31,7 +31,24 @@ else()
 
     set(OSL_INCLUDE_DIR ${OSL_ROOT}/include)
 
-    if(EXISTS "${OSL_INCLUDE_DIR}/OSL/oslversion.h")
+    set(OSL_VERSION_HEADER "${OSL_INCLUDE_DIR}/OSL/oslversion.h")
+
+    if(EXISTS "${OSL_VERSION_HEADER}")
+
+        # Try to figure out version number
+        file (STRINGS "${OSL_VERSION_HEADER}" TMP REGEX "^#define OSL_LIBRARY_VERSION_MAJOR .*$")
+        string (REGEX MATCHALL "[0-9]+" OSL_VERSION_MAJOR ${TMP})
+        file (STRINGS "${OSL_VERSION_HEADER}" TMP REGEX "^#define OSL_LIBRARY_VERSION_MINOR .*$")
+        string (REGEX MATCHALL "[0-9]+" OSL_VERSION_MINOR ${TMP})
+        file (STRINGS "${OSL_VERSION_HEADER}" TMP REGEX "^#define OSL_LIBRARY_VERSION_PATCH .*$")
+        string (REGEX MATCHALL "[0-9]+" OSL_VERSION_PATCH ${TMP})
+        file (STRINGS "${OSL_VERSION_HEADER}" TMP REGEX "^#define OSL_LIBRARY_VERSION_TWEAK .*$")
+        if (TMP)
+            string (REGEX MATCHALL "[0-9]+" OSL_VERSION_TWEAK ${TMP})
+        else ()
+            set (OSL_VERSION_TWEAK 0)
+        endif ()
+        set (OpenShadingLanguage_VERSION "${OSL_VERSION_MAJOR}.${OSL_VERSION_MINOR}.${OSL_VERSION_PATCH}.${OSL_VERSION_TWEAK}")
 
         # Find the oslcomp library.
         find_library(oslcomp_LIBRARY
@@ -77,16 +94,15 @@ else()
 
     endif()
 
-endif()
+    include (FindPackageHandleStandardArgs)
+    find_package_handle_standard_args (OpenShadingLanguage
+        FOUND_VAR     OpenShadingLanguage_FOUND
+        REQUIRED_VARS OSL_INCLUDE_DIR oslcomp_LIBRARY oslexec_LIBRARY OpenShadingLanguage_VERSION
+        VERSION_VAR   OpenShadingLanguage_VERSION
+    )
 
-###############################################################################
-### Check the C++ version ###
+    set(OSL_FOUND ${OpenShadingLanguage_FOUND})
 
-# TODO: Which version starts to impose C++14?
-
-if(${CMAKE_CXX_STANDARD} LESS_EQUAL 11)
-    set(OSL_FOUND OFF)
-    message(WARNING "Need C++14 or higher to compile OpenShadingLanguage. Skipping build the OSL unit tests")
 endif()
 
 ###############################################################################
@@ -105,6 +121,11 @@ if(OSL_FOUND)
 
     target_include_directories(osl::osl INTERFACE "${LIB_INCLUDE_DIRS}")
     target_link_libraries(osl::osl INTERFACE OSL::oslcomp OSL::oslexec)
+
+    if (${OpenShadingLanguage_VERSION} VERSION_GREATER_EQUAL "1.12" AND ${CMAKE_CXX_STANDARD} LESS_EQUAL 11)
+        set(OSL_FOUND OFF)
+        message(WARNING "Need C++14 or higher to compile OpenShadingLanguage. Skipping build the OSL unit tests")
+    endif()
 
     mark_as_advanced(OSL_INCLUDE_DIR
         oslcomp_LIBRARY oslcomp_FOUND
