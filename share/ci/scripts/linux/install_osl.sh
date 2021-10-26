b9802dada77e186ee7b8b59c314a1ffcde1d1f32
Rémi Achard <remiachard@gmail.com>
2021-10-26 16:44:34 +0100

    Install OSL latest in analysis CI
    Signed-off-by: Rémi Achard <remiachard@gmail.com>

---
+++ share/ci/scripts/linux/install_osl.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright Contributors to the OpenColorIO Project.
+
+set -ex
+
+OSL_VERSION="$1"
+
+git clone https://github.com/AcademySoftwareFoundation/OpenShadingLanguage.git
+cd OpenShadingLanguage
+
+if [ "$OSL_VERSION" == "latest" ]; then
+    LATEST_TAG=$(git describe --abbrev=0 --tags)
+    git checkout tags/${LATEST_TAG} -b ${LATEST_TAG}
+else
+    git checkout tags/Release-${OSL_VERSION} -b Release-${OSL_VERSION}
+fi
+
+mkdir build
+cd build
+cmake -DOSL_BUILD_TESTS=OFF \
+      -DVERBOSE=ON \
+      -DSTOP_ON_WARNING=OFF \
+      -DBoost_NO_BOOST_CMAKE=ON \
+      ../.
+make -j4
+sudo make install
+
+cd ../..
+rm -rf OpenShadingLanguage
