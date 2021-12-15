e64125c82b386f9c429f3cd1fe2e21dda9edc0be
Brendan Bolles <brendan@fnordware.com>
2021-12-15 14:39:50 -0800

    Bump version to 2.1.1
    Signed-off-by: Brendan Bolles <brendan@fnordware.com>

--- vendor/aftereffects/vc/vc15/OpenColorABI.h
+++ vendor/aftereffects/vc/vc15/OpenColorABI.h
@@ -7,9 +7,9 @@
 // Makefile configuration options
 #define OCIO_NAMESPACE OpenColorIO
 
-#define OCIO_VERSION_STR        "2.1.0"
+#define OCIO_VERSION_STR        "2.1.1"
 #define OCIO_VERSION_STATUS_STR ""
-#define OCIO_VERSION_FULL_STR   "2.1.0"
+#define OCIO_VERSION_FULL_STR   "2.1.1"
 
 /* Version as a single 4-byte hex number, e.g. 0x01050200 == 1.5.2
    Use this for numeric comparisons, e.g. #if OCIO_VERSION_HEX >= ... 
@@ -18,7 +18,7 @@
    */
 #define OCIO_VERSION_HEX ((2 << 24) | \
                           (1 << 16) | \
-                          (0 <<  8))
+                          (1 <<  8))
 
 #define OCIO_VERSION_MAJOR 2
 #define OCIO_VERSION_MINOR 1
