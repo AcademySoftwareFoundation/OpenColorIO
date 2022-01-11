1d8a28c2e2f842de053cef25887b7d5bc89dc438
Rémi Achard <remiachard@gmail.com>
2022-01-11 12:42:59 +0000

    Fix support for ARM64 macOS python wheels (#1574)
    * Fix support for ARM64 macOS python wheels
    
    Signed-off-by: Rémi Achard <remiachard@gmail.com>
    
    * Use $<SEMICOLON> generator expression to escape semicolon
    
    Signed-off-by: Rémi Achard <remiachard@gmail.com>
    
    Co-authored-by: Patrick Hodoul <patrickhodoul@gmail.com>

--- setup.py
+++ setup.py
@@ -60,17 +60,17 @@ class CMakeBuild(build_ext):
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
 
-        # required for auto-detection of auxiliary "native" libs
+        # required for auto-detection & inclusion of auxiliary "native" libs
         if not extdir.endswith(os.path.sep):
             extdir += os.path.sep
 
-        cfg = "Debug" if self.debug else "Release"
+        debug = int(os.environ.get("DEBUG", 0)) if self.debug is None else self.debug
+        cfg = "Debug" if debug else "Release"
 
         # CMake lets you override the generator - we need to check this.
         # Can be set with Conda-Build, for example.
         cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
 
-        # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
         cmake_args = [
             "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={}".format(extdir),
             "-DPython_EXECUTABLE={}".format(sys.executable),
@@ -81,11 +81,14 @@ class CMakeBuild(build_ext):
             "-DOCIO_BUILD_APPS=OFF",
             "-DOCIO_BUILD_TESTS=OFF",
             "-DOCIO_BUILD_GPU_TESTS=OFF",
-            # Install pybind11 via pip to avoid issue on Windows where
-            # ExternalProject pybind11 build detect the wrong version
-            "-DOCIO_INSTALL_EXT_PACKAGES=MISSING",
+            # Make sure we build everything for the requested architecture(s)
+            "-DOCIO_INSTALL_EXT_PACKAGES=ALL",
         ]
         build_args = []
+        # Adding CMake arguments set as environment variable
+        # (needed e.g. to build for ARM OSx on conda-forge)
+        if "CMAKE_ARGS" in os.environ:
+            cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
 
         if self.compiler.compiler_type != "msvc":
             # Using Ninja-build since it a) is available as a wheel and b)
@@ -94,7 +97,12 @@ class CMakeBuild(build_ext):
             # Users can override the generator with CMAKE_GENERATOR in CMake
             # 3.15+.
             if not cmake_generator:
-                cmake_args += ["-GNinja"]
+                try:
+                    import ninja  # noqa: F401
+
+                    cmake_args += ["-GNinja"]
+                except ImportError:
+                    pass
 
         else:
 
@@ -116,6 +124,12 @@ class CMakeBuild(build_ext):
                     "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
                 ]
                 build_args += ["--config", cfg]
+
+        if sys.platform.startswith("darwin"):
+            # Cross-compile support for macOS - respect ARCHFLAGS if set
+            archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
+            if archs:
+                cmake_args += ["-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
 
         # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
         # across all generators.
