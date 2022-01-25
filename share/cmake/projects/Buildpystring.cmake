9af47cbb7a3c6ce907f610faaf3905b53aa02400
Patrick Hodoul <Patrick.Hodoul@autodesk.com>
2022-01-25 13:50:00 -0500

    Adsk Contrib - Improve the library install step (#1578) (#1592)
    * Adsk Contrib - Improve the library install step
    
    Signed-off-by: Patrick Hodoul <Patrick.Hodoul@autodesk.com>
    
    * Docs compilation on *mix plateforms only
    
    Signed-off-by: Patrick Hodoul <Patrick.Hodoul@autodesk.com>
    
    * Improve the third-party library install
    
    Signed-off-by: Patrick Hodoul <Patrick.Hodoul@autodesk.com>

--- share/cmake/projects/Buildpystring.cmake
+++ share/cmake/projects/Buildpystring.cmake
@@ -6,27 +6,27 @@ project(pystring)
 cmake_minimum_required(VERSION 3.10)
 
 set(HEADERS
-	pystring.h
+    pystring.h
 )
 
 set(SOURCES
-	pystring.cpp
+    pystring.cpp
 )
 
 add_library(${PROJECT_NAME} STATIC ${HEADERS} ${SOURCES})
 
 if(UNIX)
-	set(pystring_CXX_FLAGS "${pystring_CXX_FLAGS} -fPIC")
+    set(pystring_CXX_FLAGS "${pystring_CXX_FLAGS} -fPIC")
 endif()
 
 set_target_properties(${PROJECT_NAME} PROPERTIES 
-	COMPILE_FLAGS "${PLATFORM_COMPILE_FLAGS} ${pystring_CXX_FLAGS}"
+    COMPILE_FLAGS "${PLATFORM_COMPILE_FLAGS} ${pystring_CXX_FLAGS}"
     PUBLIC_HEADER "${HEADERS}"
 )
 
 install(TARGETS ${PROJECT_NAME}
-	RUNTIME DESTINATION bin
-	LIBRARY DESTINATION lib
-	ARCHIVE DESTINATION lib
-	PUBLIC_HEADER DESTINATION include/pystring
+    RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+    LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+    ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+    PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/pystring
 )
