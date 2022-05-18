25e910cf41132c2cd45ae95360be2d7faaf7d19b
Cédrik Fuoco <105517825+cedrik-fuoco-adsk@users.noreply.github.com>
2022-05-18 11:54:32 -0400

    Adsk contrib - Fix issues when building OCIO with MS Visual Studio 2022 (#1647)
    * Fix issues when building OCIO with MS Visual Studio 2022
    
    Signed-off-by: Cedrik Fuoco <cedrik.fuoco@autodesk.com>
    
    * Removing GLEW_LIBRARY variable as it is not needed
    
    Signed-off-by: Cedrik Fuoco <cedrik.fuoco@autodesk.com>
    
    * Bumping CI to use VC 2022 as per Doug Walker request (see #1634)
    
    Signed-off-by: Cedrik Fuoco <cedrik.fuoco@autodesk.com>
    
    * Reverting the changes on ci_workflow.yml and adding a check for MSVC for the Glew fix.
    
    Signed-off-by: Cedrik Fuoco <cedrik.fuoco@autodesk.com>
    
    * Typo for the windows action in ci workflow
    
    Signed-off-by: Cedrik Fuoco <cedrik.fuoco@autodesk.com>
    
    * Typo for the windows action in ci workflow
    
    Signed-off-by: Cedrik Fuoco <cedrik.fuoco@autodesk.com>
    
    Co-authored-by: Doug Walker <doug.walker@autodesk.com>

--- share/cmake/modules/FindExtPackages.cmake
+++ share/cmake/modules/FindExtPackages.cmake
@@ -100,7 +100,8 @@ if(OCIO_BUILD_PYTHON OR OCIO_BUILD_DOCS)
     if(OCIO_BUILD_PYTHON)
         # pybind11
         # https://github.com/pybind/pybind11
-        find_package(pybind11 2.6.1 REQUIRED)
+        # pybind11 2.9 fixes issues with MS Visual Studio 2022 (Debug).
+        find_package(pybind11 2.9.2 REQUIRED)
     endif()
 endif()
 
