7afcd7ae61d2be51da597e8e734e7dede865a68b
Cédrik Fuoco <cedrik.fuoco@autodesk.com>
2023-02-16 09:18:49 -0500

    Tweaking comments, some cmake variables naming and other minors changes.
    Signed-off-by: Cédrik Fuoco <cedrik.fuoco@autodesk.com>

--- share/cmake/utils/CompilerFlags.cmake
+++ share/cmake/utils/CompilerFlags.cmake
@@ -26,7 +26,6 @@ endif()
 ###############################################################################
 # Define if SSE2 can be used.
 
-# Check for SSE2 only if the user asked for it.
 if(OCIO_USE_SSE)
     include(CheckSupportSSE2)
 endif()
