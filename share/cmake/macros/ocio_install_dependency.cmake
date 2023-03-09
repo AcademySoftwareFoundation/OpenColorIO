1d9b173e6e80e12885b4ffb5d72cb04f60da896b
Cédrik Fuoco <cedrik.fuoco@autodesk.com>
2023-03-09 11:49:17 -0500

    Add the PROMOTE_TARGET to ocio_install_dependency to support a case where ocio_install_dependency would be used without ocio_handle_dependency. Added a new function named ocio_check_dependency_version to check the version of a package without propagating any variables set by find_package. Fix for Linux CI.
    Signed-off-by: Cédrik Fuoco <cedrik.fuoco@autodesk.com>

--- share/cmake/macros/ocio_install_dependency.cmake
+++ share/cmake/macros/ocio_install_dependency.cmake
@@ -18,17 +18,29 @@ macro (ocio_install_dependency dep_name)
         # options
         ""
         # one value keywords
-        "VERSION"
+        "VERSION;PROMOTE_TARGET"
         # multi value keywords
         ""
         # args
         ${ARGN})
+
+
     if(NOT ${dep_name}_FOUND AND OCIO_INSTALL_EXT_PACKAGES AND NOT OCIO_INSTALL_EXT_PACKAGES STREQUAL NONE)
         set(OCIO_${dep_name}_RECOMMENDED_VERSION ${ocio_id_VERSION})
         include(Install${dep_name})
+        set(_${dep_name}_built_by_ocio TRUE)
     endif()
 
     if(${dep_name}_FOUND)
-        message(STATUS "${ColorSuccess}Installed ${dep_name} (version \"${ocio_id_VERSION}\")${ColorReset}")
+        if(${dep_name}_FOUND AND ocio_id_PROMOTE_TARGET)
+            foreach (_target_to_be_promoted_ ${ocio_id_PROMOTE_TARGET})
+                set_target_properties(${_target_to_be_promoted_} PROPERTIES IMPORTED_GLOBAL TRUE)
+            endforeach()
+        endif()
+
+        if (${_${dep_name}_built_by_ocio})
+           message(STATUS "${ColorSuccess}Installed ${dep_name} (version \"${ocio_id_VERSION}\")${ColorReset}")
+        endif()
     endif()
+    unset(_${dep_name}_built_by_ocio)
 endmacro()
