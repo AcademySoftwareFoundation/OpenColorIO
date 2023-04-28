4271acc1d0473246eb5d21a03a03f2eaa7a0757b
Cédrik Fuoco <cedrik.fuoco@autodesk.com>
2023-04-28 09:46:06 -0400

    Adding LLVM_ROOT for macOS only and testing the usage of "llvm" only instead of llvm@15.
    Signed-off-by: Cédrik Fuoco <cedrik.fuoco@autodesk.com>

--- share/ci/scripts/multi/install_osl.sh
+++ share/ci/scripts/multi/install_osl.sh
@@ -22,17 +22,28 @@ mkdir build
 cd build
 # FIXME: Revert OSL_BUILD_TESTS to OFF when OSL 1.12 is released
 # CMake configure fails when tests are off, only fixed in 1.12 dev branch
-cmake -DCMAKE_BUILD_TYPE=Release \
-      ${INSTALL_TARGET:+"-DCMAKE_INSTALL_PREFIX="${INSTALL_TARGET}""} \
-      -DCMAKE_CXX_STANDARD=14 \
-      -DCMAKE_C_COMPILER=$(brew --prefix llvm@15)/bin/clang \
-      -DCMAKE_CXX_COMPILER=$(brew --prefix llvm@15)/bin/clang++ \
-      -DOSL_BUILD_TESTS=ON \
-      -DVERBOSE=ON \
-      -DSTOP_ON_WARNING=OFF \
-      -DBoost_NO_BOOST_CMAKE=ON \
-      -DLLVM_ROOT=$(brew --prefix llvm@15) \
-      ../.
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+    cmake -DCMAKE_BUILD_TYPE=Release \
+        ${INSTALL_TARGET:+"-DCMAKE_INSTALL_PREFIX="${INSTALL_TARGET}""} \
+        -DCMAKE_CXX_STANDARD=14 \
+        -DOSL_BUILD_TESTS=ON \
+        -DVERBOSE=ON \
+        -DSTOP_ON_WARNING=OFF \
+        -DBoost_NO_BOOST_CMAKE=ON \
+        -DLLVM_ROOT=$(brew --prefix llvm) \
+        ../.
+else # not macOS
+    cmake -DCMAKE_BUILD_TYPE=Release \
+        ${INSTALL_TARGET:+"-DCMAKE_INSTALL_PREFIX="${INSTALL_TARGET}""} \
+        -DCMAKE_CXX_STANDARD=14 \
+        -DOSL_BUILD_TESTS=ON \
+        -DVERBOSE=ON \
+        -DSTOP_ON_WARNING=OFF \
+        -DBoost_NO_BOOST_CMAKE=ON \
+        ../.
+fi
+
 cmake --build . \
       --target install \
       --config Release \
