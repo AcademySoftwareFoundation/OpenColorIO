32de54cbf6f76da7d0045bcdccb72beb00c96a09
JGamache-autodesk <56274617+JGamache-autodesk@users.noreply.github.com>
2025-03-10 14:53:27 -0400

    Integrating matrix multiplication fix from OSL (#2121)
    See https://github.com/AcademySoftwareFoundation/OpenShadingLanguage/pull/1513 for more details.
    
    Signed-off-by: Jerry Gamache <jerry.gamache@autodesk.com>
    Co-authored-by: Doug Walker <doug.walker@autodesk.com>
    (cherry picked from commit fed973f463477f71c56304432306f99508d3754f)
    Signed-off-by: Doug Walker <doug.walker@autodesk.com>

--- src/OpenColorIO/GpuShaderClassWrapper.cpp
+++ src/OpenColorIO/GpuShaderClassWrapper.cpp
@@ -66,10 +66,7 @@ std::string OSLShaderClassWrapper::getClassWrapperHeader(const std::string& orig
     st.newLine() << "vector4 __operator__mul__(matrix m, vector4 v)";
     st.newLine() << "{";
     st.indent();
-    st.newLine() << "return vector4(v.x * m[0][0] + v.y * m[0][1] + v.z * m[0][2] + v.w * m[0][3], ";
-    st.newLine() << "               v.x * m[1][0] + v.y * m[1][1] + v.z * m[1][2] + v.w * m[1][3], ";
-    st.newLine() << "               v.x * m[2][0] + v.y * m[2][1] + v.z * m[2][2] + v.w * m[2][3], ";
-    st.newLine() << "               v.x * m[3][0] + v.y * m[3][1] + v.z * m[3][2] + v.w * m[3][3]);";
+    st.newLine() << "return transform(m, v);";
     st.dedent();
     st.newLine() << "}";
 
