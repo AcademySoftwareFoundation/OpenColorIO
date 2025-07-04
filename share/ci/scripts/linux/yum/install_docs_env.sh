83a45b00b90b7393ad849c491f1384d18317fe79
Michael Dolan <michdolan@gmail.com>
2025-07-03 23:09:16 -0400

    Update CI to use windows-2022, and linux builds to use apt
    Signed-off-by: Michael Dolan <michdolan@gmail.com>

--- share/ci/scripts/linux/yum/install_docs_env.sh
+++ share/ci/scripts/linux/yum/install_docs_env.sh
@@ -6,11 +6,5 @@ set -ex
 
 HERE=$(dirname $0)
 
-# The yum install doxygen command started failing during CI since mirrorlist.centos.org
-# was turned off. However, it seems that Doxygen is already installed on the containers
-# used for CI, so this command is not currently necessary there. You will need to
-# uncomment this line if you want to build the documentation on Linux but don't have
-# doxygen already installed.
-# bash $HERE/install_doxygen.sh latest
-
+bash $HERE/install_doxygen.sh latest
 pip install -r $HERE/../../../../../docs/requirements.txt
