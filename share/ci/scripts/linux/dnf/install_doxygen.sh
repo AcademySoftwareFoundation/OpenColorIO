f75dca1030b9a4f063031f93eedcf2e0dd5fba02
Michael Dolan <michdolan@gmail.com>
2025-07-05 17:39:25 -0400

    Turn off docs on CentOS-7 builds
    Signed-off-by: Michael Dolan <michdolan@gmail.com>

--- share/ci/scripts/linux/dnf/install_doxygen.sh
+++ share/ci/scripts/linux/dnf/install_doxygen.sh
@@ -6,13 +6,10 @@ set -ex
 
 DOXYGEN_VERSION="$1"
 
-if command -v dnf >/dev/null; then
+if command -v doxygen >/dev/null; then
     if [ "$DOXYGEN_VERSION" == "latest" ]; then
         dnf install -y doxygen
     else
         dnf install -y doxygen-${DOXYGEN_VERSION}
     fi
-elif command -v doxygen >/dev/null; then
-    # Get any working version...
-    yum install -y doxygen
 fi
