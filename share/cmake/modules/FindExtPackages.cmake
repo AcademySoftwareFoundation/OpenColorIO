1ae669e6020d493da3d5c8e5e446334f333d085d
Doug Walker <doug.walker@autodesk.com>
2025-09-16 19:00:11 -0400

    Minor clean up
    Signed-off-by: Doug Walker <doug.walker@autodesk.com>

--- share/cmake/modules/FindExtPackages.cmake
+++ share/cmake/modules/FindExtPackages.cmake
@@ -184,7 +184,6 @@ if(OCIO_BUILD_PYTHON OR OCIO_BUILD_DOCS)
 endif()
 
 # Set OpenEXR Minimum version as a variable since it it used at multiple places.
-# set(OpenEXR_MININUM_VERSION "3.0.5")
 set(OpenEXR_MININUM_VERSION "3.1.6")
 if((OCIO_BUILD_APPS AND OCIO_USE_OIIO_FOR_APPS) OR OCIO_BUILD_TESTS)
     # OpenImageIO is required for OSL unit test and optional for apps.
