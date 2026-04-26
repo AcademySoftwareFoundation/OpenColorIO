07afd2857310bf8bde7e28407e8440c1c0ab1952
Eric Renaud-Houde <eric.renaud.houde@gmail.com>
2026-04-26 11:54:09 -0400

    Use OCIO_DirectX-Headers_RECOMMENDED_VERSION in InstallDirectX-Headers.cmake
      ocio_install_dependency already propagates the RECOMMENDED_VERSION from the
      ocio_handle_dependency call site. Consume it instead of hardcoding the
      version a second time. Matches the pattern in Installyaml-cpp.cmake and
      Installpystring.cmake.
    
    Signed-off-by: Eric Renaud-Houde <eric.renaud.houde@gmail.com>

--- share/cmake/modules/install/InstallDirectX-Headers.cmake
+++ share/cmake/modules/install/InstallDirectX-Headers.cmake
@@ -8,12 +8,18 @@
 
 include(FetchContent)
 
+if(OCIO_DirectX-Headers_RECOMMENDED_VERSION)
+    set(DirectX-Headers_VERSION ${OCIO_DirectX-Headers_RECOMMENDED_VERSION})
+else()
+    set(DirectX-Headers_VERSION ${DirectX-Headers_FIND_VERSION})
+endif()
+
 set(FETCHCONTENT_BASE_DIR "${CMAKE_BINARY_DIR}/ext/build/DirectX-Headers")
 set(DIRECTX_HEADERS_BUILD_TEST OFF CACHE BOOL "" FORCE)
 
 FetchContent_Declare(DirectX-Headers
     GIT_REPOSITORY https://github.com/microsoft/DirectX-Headers.git
-    GIT_TAG        v1.619.1
+    GIT_TAG        v${DirectX-Headers_VERSION}
 )
 
 FetchContent_MakeAvailable(DirectX-Headers)
@@ -23,5 +29,4 @@ FetchContent_MakeAvailable(DirectX-Headers)
 # the Microsoft::DirectX-Headers target via the upstream CMakeLists.
 if(TARGET Microsoft::DirectX-Headers)
     set(DirectX-Headers_FOUND TRUE)
-    set(DirectX-Headers_VERSION "1.619.1")
 endif()
